df9129e6fece0a86603ced49baf941ed8a9091ff
Polina <polinka29enp@gmail.com>
2022-08-07 14:03:00 +0300

    массив

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,14 @@
+﻿// Нахождение макс числа из 9 с помощью массива и функций
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+// идекс массива 0  1    2   3  4  5   6  7   8
+int [] array = {19, 27, 38, 4, 55, 6, 79, 80, 91};
+array[0] = 12;
+Console.WriteLine(array[4]);
